0a95999a425a151a36244d79017996e91f637da8
brinjaul02 <463232683@qq.com>
2020-08-19 14:45:01 +0800

    完成无filter 解析插入自己定义表里

--- lib/logstash/outputs/mycrassandra.rb
+++ lib/logstash/outputs/mycrassandra.rb
@@ -5,14 +5,15 @@ require 'json'
 
 # An mycrassandra output that does nothing.
 class LogStash::Outputs::Mycrassandra < LogStash::Outputs::Base
-  attr_accessor :result, :keyspace, :cluster, :session,:table
+  attr_accessor :result, :keyspace, :cluster, :session, :table
   config_name "mycrassandra"
   config :cra_database, :validate => :string, :default => "bootwiki"
   config :cra_table, :validate => :string, :default => "zp"
   config :cra_name, :validate => :string, :default => "root"
   config :cra_pwd, :validate => :string, :default => "123456"
   config :cra_hosts, :validate => :string, :default => "192.168.244.10"
-  @@calc_count=0
+  @@calc_count = 0
+
   public
 
   def register
@@ -21,7 +22,7 @@ class LogStash::Outputs::Mycrassandra < LogStash::Outputs::Base
                                  password: cra_pwd,
                                  hosts: [cra_hosts])
     @keyspace = cra_database
-    @table= cra_table
+    @table = cra_table
     @session = @cluster.connect(@keyspace) # create session, optionally scoped to a keyspace, to execute queries
     puts "=========end  register==================="
   end
@@ -31,31 +32,48 @@ class LogStash::Outputs::Mycrassandra < LogStash::Outputs::Base
 
   def receive(event)
     puts "========begin receive========================="
-    myobject = {}.merge(event.to_hash)
-    #puts "myobject:===================#{myobject}"
+    begin
+      myobject = {}.merge(event.to_hash)
+      puts "myobject:====#{myobject.class}===============#{myobject}"
+    rescue Exception => e
 
-    mydoc = myobject['doc']
+      puts "ex============#{ e.message }"
+      raise e
+    end
+    messh = myobject['message']
+    mess = messh.to_s
+    puts "begin==#{mess.class}========mess=====================>#{mess}<============mess=====================end"
+    arr_log = JSON.parse mess.gsub(/\\/, '')
+    puts "check arr_log-class #{arr_log.class}========test element: #{arr_log[0]['traceId']} "
+    for mydoc in arr_log
 
-    puts "myobject['doc']:======#{mydoc.class}=============#{myobject['doc']}"
 
-    puts "=after=mydoc['localEndpoint']==============>#{mydoc['localEndpoint']}"
-    local_p = check_lp(mydoc)
-    puts "=after=mydoc['localEndpoint']==============>#{mydoc['localEndpoint']}==============local_p=====>#{local_p}"
 
-    puts "before==mydoc['remoteEndpoint']==============>#{mydoc['remoteEndpoint']}"
-    remove_p = check_rp(mydoc)
-    puts "after==mydoc['remoteEndpoint']============>#{mydoc['remoteEndpoint']}==========>remove_p:======>#{remove_p}"
+      puts "mydoc-elements:======#{mydoc.class}=============#{mydoc['traceId']}"
 
-    tags_p =check_tags(mydoc)
-    puts "after==mydoc['tags']============>#{mydoc['tags']}====>tags_p:======>#{tags_p}"
-    sql_cra = "INSERT INTO #{@table} (traceId,name,parentId,id,kind,timestamp,timestamp_mills,duration,debug,shared,localEndpoint,remoteEndpoint,tags) VALUES ('#{mydoc['traceId'] == nil ? 'null' : mydoc['traceId']}','#{mydoc['name'] == nil ? 'null' : mydoc['name']}','#{mydoc['parentId'] == nil ? 'null' : mydoc['parentId']}','#{mydoc['id'] == nil ? 'null' : mydoc['id']}','#{mydoc['kind'] == nil ? 'null' : mydoc['kind']}',#{mydoc['timestamp'] == nil ? 'null' : mydoc['timestamp']},#{mydoc['timestamp_mills'] == nil ? 'null' : mydoc['timestamp_mills']},#{mydoc['duration'] == nil ? 'null' : mydoc['duration']},#{mydoc['debug'] == nil ? 'null' : mydoc['debug']},#{mydoc['shared'] == nil ? 'null' : mydoc['shared']},#{local_p},#{remove_p},#{tags_p})"
-    puts sql_cra
-    @session.execute(sql_cra) # fully asynchronous api
-    @@calc_count += 1
-    puts  "current calc_count:===>#@@calc_count"
+      puts "=after=mydoc['localEndpoint']==============>#{mydoc['localEndpoint']}"
+      local_p = check_lp(mydoc)
+      puts "=after=mydoc['localEndpoint']==============>#{mydoc['localEndpoint']}==============local_p=====>#{local_p}"
+
+      puts "before==mydoc['remoteEndpoint']==============>#{mydoc['remoteEndpoint']}"
+      remove_p = check_rp(mydoc)
+      puts "after==mydoc['remoteEndpoint']============>#{mydoc['remoteEndpoint']}==========>remove_p:======>#{remove_p}"
+
+      tags_p = check_tags(mydoc)
+      puts "after==mydoc['tags']============>#{mydoc['tags']}====>tags_p:======>#{tags_p}"
+      sql_cra = "INSERT INTO #{@table} (traceId,name,parentId,id,kind,timestamp,timestamp_mills,duration,debug,shared,localEndpoint,remoteEndpoint,tags) VALUES ('#{mydoc['traceId'] == nil ? 'null' : mydoc['traceId']}','#{mydoc['name'] == nil ? 'null' : mydoc['name']}','#{mydoc['parentId'] == nil ? 'null' : mydoc['parentId']}','#{mydoc['id'] == nil ? 'null' : mydoc['id']}','#{mydoc['kind'] == nil ? 'null' : mydoc['kind']}',#{mydoc['timestamp'] == nil ? 'null' : mydoc['timestamp']},#{mydoc['timestamp_mills'] == nil ? 'null' : mydoc['timestamp_mills']},#{mydoc['duration'] == nil ? 'null' : mydoc['duration']},#{mydoc['debug'] == nil ? 'null' : mydoc['debug']},#{mydoc['shared'] == nil ? 'null' : mydoc['shared']},#{local_p},#{remove_p},#{tags_p})"
+      puts sql_cra
+      @session.execute(sql_cra) # fully asynchronous api
+      @@calc_count += 1
+      puts "current calc_count:===>#@@calc_count"
+    end
+
     puts "========end receive========================="
+
     return "event  received"
-  end # def event
+  end
+
+  # def event
 
   def check_lp(mydoc)
     begin
@@ -64,7 +82,7 @@ class LogStash::Outputs::Mycrassandra < LogStash::Outputs::Base
         mydoc['localEndpoint']['port'] = "#{pore}"
         local_p = mydoc['localEndpoint'].to_json.gsub!(/["]/, '\'') == nil ? 'null' : mydoc['localEndpoint'].to_json.gsub!(/["]/, '\'')
       else
-        local_p ='null'
+        local_p = 'null'
       end
     rescue Exception => e
       puts "check  localEndpoint"
